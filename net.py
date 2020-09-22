3aa4564f9def40e969077002a2dd81fe5fe97c95
Nikola Milosevic <nikolam495@gmail.com>
2020-09-22 15:23:28 +0200

    add experiments notebook

--- net.py
+++ net.py
@@ -1,30 +1,15 @@
+import os
 import torch
 import torch.nn as nn
 import numpy as np
 from numpy import pi, ndarray
 import matplotlib.pyplot as plt
-import torch.optim as optim
 from matplotlib import figure
 from matplotlib.collections import LineCollection
 from torch.autograd.functional import jacobian
 from matplotlib.patches import Ellipse
 from itertools import zip_longest
-
-
-# Quick config
-learning_rate = 0.01
-num_layers = 10
-num_epochs = 500
-N = 1000
-identity = True  # identity connection between layer input and layer output?
-randomize_data = False  # add gaussian to point position on the data spiral
-init_gain = 0.001
-wd = 0.8  # weight decay to keep delta l small?
-grid_dim = 10  # number of grid points per axis
-spiral_length_rad = 2*pi
-activation = torch.tanh
-plot_grids = True
-c2 = False
+from sklearn.datasets import make_circles, make_moons
 
 
 class RiemannianMetric:
@@ -53,6 +38,12 @@ class RiemannianMetric:
         return self
 
 
+class CustomLoss(nn.BCEWithLogitsLoss):
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        return torch.functional.F.binary_cross_entropy_with_logits(input, target, self.weight,
+                                                                   pos_weight=self.pos_weight, reduction=self.reduction)
+
+
 class Layer(nn.Module):
     def __init__(self, act_func, identity_conn=False):
         super(Layer, self).__init__()
@@ -71,9 +62,15 @@ class Layer(nn.Module):
             return self.act_func(self.linear_map(x))
 
 
+def print_ln(ll, obj='grid'):
+    print(f'Plotting {obj} for Layer {ll}')
+
+
 class Net(nn.Module):
-    def __init__(self, layers: int, identity_conn=False):
+    def __init__(self, layers: int, identity_conn: bool, c2: bool, init_gain: float, activation):
         super(Net, self).__init__()
+        self.num_layers = layers
+        self.init_gain = init_gain
         self.activations = []
         self.jacobians = []
         self.linear_out = nn.Linear(2, 1)
@@ -118,7 +115,7 @@ class Net(nn.Module):
     def init_weights(self):
         def init(m):
             if type(m) == nn.Linear:
-                torch.nn.init.xavier_normal_(m.weight, gain=init_gain)
+                torch.nn.init.xavier_normal_(m.weight, gain=self.init_gain)
                 m.bias.data.fill_(0)
         self.apply(init)
 
@@ -149,8 +146,7 @@ class Net(nn.Module):
                 # y = 2*x(l) + f(x(l);l) - x(l-1)
                 a += x - prev_layer
             if save_activations:
-                # just using 'layer' here should be ok, because the jacobian
-                # doesn't depend on the additive term from l-1. (but maybe 2*layer)
+                # not sure about this yet
                 self.save_forward(x, layer)
             prev_layer = x
             x = a
@@ -159,7 +155,7 @@ class Net(nn.Module):
     def forward(self, x: torch.Tensor, save_activations: bool = False) -> torch.Tensor:
         return self.forward_func(x, save_activations)
 
-    def plot_points(self, plots: figure, cls_a: ndarray, cls_b: ndarray) -> None:
+    def plot_points(self, plots: figure, cls_a: ndarray, cls_b: ndarray, plot_classifier: bool = False) -> None:
         # forward pass data points
         iter_plots = iter(plots)
         a = torch.from_numpy(cls_a).float()
@@ -173,22 +169,22 @@ class Net(nn.Module):
         plot.scatter(cls_a[:, 0], cls_a[:, 1])
         plot.scatter(cls_b[:, 0], cls_b[:, 1])
         for ln, layer_activations in enumerate(zip(act_cls1, act_cls2), start=1):
+            print_ln(ln, 'points')
             cls1, cls2 = map(lambda t: t.detach().numpy(), layer_activations)
             plot = next(iter_plots)
             plot.scatter(cls1[:, 0], cls1[:, 1])
             plot.scatter(cls2[:, 0], cls2[:, 1])
-            if ln == num_layers:
+            if ln == self.num_layers:
                 cls12 = np.append(cls1, cls2)
                 xmin = np.min(cls12)
                 xmax = np.max(cls12)
                 params = self.parameters()
                 w = next(params).detach().numpy()
                 b = next(params).detach().numpy()
-                # Net.plot_liner_classifier(plot, w, b, xmin, xmax)
+                if plot_classifier:
+                    Net.plot_liner_classifier(plot, w, b, xmin, xmax)
 
-    def plot_grids(self, plots: figure, xmin, xmax, grid_dim_x: int, grid_dim_y: int) -> None:
-        def print_ln(ll):
-            print(f'Plotting grid for Layer {ll}')
+    def plot_grids(self, plots: figure, xmin, xmax, grid_dim_x: int, grid_dim_y: int, plot_tensors: bool) -> None:
         # calculate grid points
         iter_plots = iter(plots)
         grid_size = grid_dim_x * grid_dim_y
@@ -198,20 +194,21 @@ class Net(nn.Module):
         # forward pass grid points
         self.forward(grid_tensor, save_activations=True)
         plot = next(iter_plots)
+        print_ln(0)
         Net.plot_grid(grid_x, grid_y, ax=plot, color="lightgrey")
-        for e, grid in enumerate(self.activations):
-            print_ln(e)
+        for e, grid in enumerate(self.activations, start=1):
             plot = next(iter_plots)
+            print_ln(e)
             grid = grid.detach().numpy()
             xx = grid.T.reshape(2, grid_dim_x, grid_dim_y)[0]
             yy = grid.T.reshape(2, grid_dim_x, grid_dim_y)[1]
             Net.plot_grid(xx, yy, ax=plot, color="lightgrey")
-        self.plot_tensors(plots, grid_numpy_array, grid_tensor)
+        if plot_tensors:
+            self.plot_tensors(plots, grid_numpy_array, grid_tensor)
 
     def plot_tensors(self, plots: figure, grid_numpy_array: ndarray, grid_tensor: torch.Tensor) -> None:
         # every point gets it's own color for the metric tensor plot
         cmap = Net.get_cmap(len(grid_numpy_array))
-        metric_tensors = []
         print("Plotting tensor glyphs...")
         for e, grid_point in enumerate(grid_numpy_array):
             point = grid_tensor[e]
@@ -219,7 +216,8 @@ class Net(nn.Module):
             g = RiemannianMetric()
             g_numpy = g.matrix
             iter_jacobi = reversed(self.jacobians)
-            for en, layer in enumerate(zip_longest(reversed(self.activations), reversed(plots), fillvalue=torch.from_numpy(grid_point))):
+            for en, layer in enumerate(zip_longest(reversed(self.activations), reversed(plots),
+                                                   fillvalue=torch.from_numpy(grid_point))):
                 point, plot = layer
                 x, y = point.detach().numpy()
                 if en != 0:
@@ -233,57 +231,69 @@ class Net(nn.Module):
                 plot.add_artist(Ellipse((x, y), width, height, angle * 360 / (2 * pi),
                                         zorder=3, facecolor=cmap(e), edgecolor='k', lw=0.5))
 
-    def plot_geometry(self, a_numpy, b_numpy, grid_dim_x=grid_dim, grid_dim_y=grid_dim):
+    def plot_geometry(self, a_numpy, b_numpy, grid_dim_x=15, grid_dim_y=15, plot_rows=1, plot_cols=11,
+                      plot_grids=False, plot_points=True, plot_tensors=False, plot_classifier=False, save_img='test'):
         """
         @param a_numpy:
         @param b_numpy:
         @param grid_dim_x:
         @param grid_dim_y:
+        @param plot_rows:
+        @param plot_cols:
+        @param plot_grids:
+        @param plot_points:
+        @param plot_tensors:
+        @param plot_classifier:
         @return:
         """
         # prepare plots
-        fig, plots = plt.subplots(1, num_layers + 1, figsize=[5 * num_layers, 5])
-
+        fig, plots = plt.subplots(plot_rows, plot_cols, figsize=[5 * plot_cols, 5 * plot_rows])
+        if plot_rows > 1:
+            plots = plots.flatten()
         xmin = min(np.min(a_numpy), np.min(b_numpy))
         xmax = max(np.max(a_numpy), np.max(b_numpy))
 
-        self.plot_points(plots, a_numpy, b_numpy)
         if plot_grids:
-            self.plot_grids(plots, xmin, xmax, grid_dim_x, grid_dim_y)
+            self.plot_grids(plots, xmin, xmax, grid_dim_x, grid_dim_y, plot_tensors)
+
+        if plot_points:
+            self.plot_points(plots, a_numpy, b_numpy, plot_classifier)
 
         for e, plot in enumerate(plots):
             plot.set_title(f'Layer {e}')
 
+        if not os.path.exists('img_out'):
+            os.mkdir('img_out')
+        i = 0
+        while os.path.exists(f'img_out/{save_img}-{i}.png'):
+            i += 1
+        plt.savefig(f'img_out/{save_img}-{i}')
         plt.show()
 
 
-def spiral2d(data_points: int = 200, randomize: bool = False) -> tuple:
+def spiral2d(data_points: int, n_noise: float, spiral_length_rad: float) -> tuple:
     """
     @note idea from https://gist.github.com/45deg
     @param data_points:
-    @param randomize:
+    @param n_noise:
+    @param spiral_length_rad:
     @return:
     """
     dims = 2
+    points_per_cls = int(data_points/2)
 
-    def add_noise(r: bool) -> ndarray:
-        if r:
-            return np.random.randn(data_points, dims)
-        else:
-            return np.zeros((data_points, dims))
-
-    theta = np.sqrt(np.random.rand(data_points))*spiral_length_rad  # np.linspace(0,2*pi,100)
+    theta = np.sqrt(np.random.rand(points_per_cls))*spiral_length_rad
 
     r_a = 2*theta + pi
     data_a = np.array([np.cos(theta)*r_a, np.sin(theta)*r_a]).T
-    x_a = data_a + add_noise(randomize)
+    x_a = data_a + n_noise * np.random.randn(points_per_cls, dims)
 
     r_b = -2*theta - pi
     data_b = np.array([np.cos(theta)*r_b, np.sin(theta)*r_b]).T
-    x_b = data_b + add_noise(randomize)
+    x_b = data_b + n_noise * np.random.randn(points_per_cls, dims)
 
-    res_a = np.append(x_a, np.zeros((data_points, 1)), axis=1)
-    res_b = np.append(x_b, np.ones((data_points, 1)), axis=1)
+    res_a = np.append(x_a, np.zeros((points_per_cls, 1)), axis=1)
+    res_b = np.append(x_b, np.ones((points_per_cls, 1)), axis=1)
 
     res = np.append(res_a, res_b, axis=0)
     np.random.shuffle(res)
@@ -295,8 +305,9 @@ def accuracy(output: torch.Tensor, target: torch.Tensor) -> float:
     correct = 0
     incorrect = 0
     total = 0
-    output = output.view(N * 2).detach().numpy()
-    target = target.view(N * 2).detach().numpy()
+    n = output.shape[0]
+    output = output.view(n).detach().numpy()
+    target = target.view(n).detach().numpy()
     for z in zip(output, target):
         o, t = z
         correct_positive = o > 0 and t == 1
@@ -309,34 +320,9 @@ def accuracy(output: torch.Tensor, target: torch.Tensor) -> float:
     return correct / total
 
 
-def main():
-    net = Net(num_layers, identity_conn=identity)
-
-    net.init_weights()
-
-    optimizer = optim.AdamW(net.parameters(), lr=learning_rate, weight_decay=wd)
-    loss_func = nn.BCEWithLogitsLoss()
-
-    data, x_a, x_b = spiral2d(N, randomize=randomize_data)
-
-    input_data = torch.from_numpy(data[:, :2]).float()
-    target = torch.from_numpy(data[:, 2]).float().view(N*2, 1)
-
-    for epoch in range(1, num_epochs):
-        optimizer.zero_grad()
-        output = net(input_data)
-        loss = loss_func(output, target)
-        loss.backward()
-        optimizer.step()
-        if epoch % (num_epochs / 10) == 0:
-            with torch.no_grad():
-                acc = accuracy(output, target)
-            print(f"epoch = {epoch}", end=", ")
-            print(f"batch loss = {loss.item()}", end=", ")
-            print(f"accuracy = {acc}")
-
-    net.plot_geometry(x_a, x_b)
-
-
-if __name__ == "__main__":
-    main()
+def data(n_samples: int, n_noise: float, n_shuffle: bool, data_type='circles'):
+    if data_type == 'moons':
+        return make_moons(n_samples=n_samples, noise=n_noise, shuffle=n_shuffle)
+    if data_type != 'circles':
+        print("Warning: Only 'spiral', 'moons' or 'circles' are admissible data manifolds. Default will be used.")
+    return make_circles(n_samples=n_samples, noise=n_noise, shuffle=n_shuffle)
